1c2623ac5e54387f3d79014165f70b00ab696644
mhselva <62720541+mhselva@users.noreply.github.com>
2021-03-31 15:29:18 -0400

    Added Poke-a-Dot (Assignment A13)

---
+++ 2900/A13Poke-A-Dot/game.js
@@ -0,0 +1,267 @@
+/*
+game.js for Perlenspiel 3.3.x
+Last revision: 2021-03-24 (BM)
+
+The following comment lines are for JSHint <https://jshint.com>, a tool for monitoring code quality.
+You may find them useful if your development environment is configured to support JSHint.
+If you don't use JSHint (or are using it with a configuration file), you can safely delete these lines.
+*/
+
+/* jshint browser : true, devel : true, esversion : 6, freeze : true */
+/* globals PS : true */
+
+"use strict"; // Do NOT delete this directive!
+
+/*
+PS.init( system, options )
+Called once after engine is initialized but before event-polling begins.
+This function doesn't have to do anything, although initializing the grid dimensions with PS.gridSize() is recommended.
+If PS.grid() is not called, the default grid dimensions (8 x 8 beads) are applied.
+Any value returned is ignored.
+[system : Object] = A JavaScript object containing engine and host platform information properties; see API documentation for details.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+*/
+
+/*
+-Prevent 2 dots from being in the same spot
+-Prevent dot from not moving
+ */
+
+const D = {
+
+	// Constants
+	GRID_WIDTH: 5,
+	GRID_HEIGHT: 5,
+	COLOR_BG: PS.COLOR_WHITE,
+	DOT_SOUND: "fx_pop",
+
+	// init - Initializes the grid, status text, dots, and handles Login information.
+	init: function (system, options) {
+
+		// Team name.
+		const TEAM = "fishbowl";
+
+		// Initializing grid.
+		PS.gridSize(D.GRID_WIDTH, D.GRID_HEIGHT);
+		PS.color(PS.ALL, PS.ALL, D.COLOR_BG);
+		PS.gridColor(D.COLOR_BG)
+		PS.radius(PS.ALL, PS.ALL, 50);
+		PS.border(PS.ALL, PS.ALL, 0);
+		PS.fade(PS.ALL, PS.ALL, 20);
+
+		// initDot - Creates the initial dots.
+		let initDot = function () {
+			let dotColor = [PS.random( 200 ) + 20, PS.random( 200 ) + 20, PS.random( 200 ) + 20];
+			let dotPosition = [(PS.random(D.GRID_WIDTH) - 1), (PS.random(D.GRID_HEIGHT) - 1)]
+
+			PS.scale(dotPosition[0], dotPosition[1], PS.random(50) + 50);
+			PS.color(dotPosition[0], dotPosition[1], dotColor);
+		};
+
+		// Initialize dots.
+		initDot();
+		initDot();
+		initDot();
+		initDot();
+		initDot();
+		initDot();
+		initDot();
+
+		// Status text.
+		PS.statusColor(PS.COLOR_BLACK);
+		PS.statusText("Poke-a-Dot");
+
+		// Login information.
+		PS.dbLogin("imgd2900", TEAM, function (id, user) {
+			if (user === PS.ERROR) {
+				return PS.dbErase(TEAM);
+			}
+			PS.dbEvent(TEAM, "startup", user);
+			PS.dbSave(TEAM, PS.CURRENT, {discard: true});
+		}, {active: false});
+	},
+
+	// touch - Handles touch events, updating dot position after a successful touch.
+	touch: function (x, y, data, options) {
+
+		// updateDot - Updates dot location & color after being clicked.
+		let updateDot = function (oldX, oldY) {
+
+			// Check to make sure the dot's position changes.
+			let dotPosition = [0,0];
+			let newX = (PS.random(D.GRID_WIDTH) - 1);
+			let newY = (PS.random(D.GRID_HEIGHT) - 1);
+
+			if (newX !== oldX || newY !== oldY) {
+				dotPosition[0] = newX;
+				dotPosition[1] = newY;
+			} else {
+				// PS.debug("Dot tried to stay in place.\n");
+				updateDot(oldX, oldY);
+				return 0; // End function early to prevent multiple dots from being created.
+			}
+
+			// Create a new dot if the location isn't already occupied by a dot.
+			if (PS.color(newX, newY) === PS.COLOR_WHITE) {
+				let dotColor = [PS.random(200) + 20, PS.random(200) + 20, PS.random(200) + 20];
+				PS.scale(dotPosition[0], dotPosition[1], PS.random(50) + 50);
+				PS.color(dotPosition[0], dotPosition[1], dotColor);
+			} else {
+				// PS.debug("Dot tried to move to an occupied space.\n");
+				updateDot(oldX, oldY);
+			}
+		};
+
+		if (PS.color(x, y) !== PS.COLOR_WHITE) {
+			PS.audioPlay(D.DOT_SOUND);
+			PS.color(x, y, PS.COLOR_WHITE);
+			updateDot(x, y);
+		}
+	}
+};
+
+PS.init = D.init;
+PS.touch = D.touch;
+
+/*
+PS.release ( x, y, data, options )
+Called when the left mouse button is released, or when a touch is lifted, over bead(x, y).
+This function doesn't have to do anything. Any value returned is ignored.
+[x : Number] = zero-based x-position (column) of the bead on the grid.
+[y : Number] = zero-based y-position (row) of the bead on the grid.
+[data : *] = The JavaScript value previously associated with bead(x, y) using PS.data(); default = 0.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+*/
+
+PS.release = function( x, y, data, options ) {
+	// Uncomment the following code line to inspect x/y parameters:
+
+	// PS.debug( "PS.release() @ " + x + ", " + y + "\n" );
+
+	// Add code here for when the mouse button/touch is released over a bead.
+};
+
+/*
+PS.enter ( x, y, button, data, options )
+Called when the mouse cursor/touch enters bead(x, y).
+This function doesn't have to do anything. Any value returned is ignored.
+[x : Number] = zero-based x-position (column) of the bead on the grid.
+[y : Number] = zero-based y-position (row) of the bead on the grid.
+[data : *] = The JavaScript value previously associated with bead(x, y) using PS.data(); default = 0.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+*/
+
+PS.enter = function( x, y, data, options ) {
+	// Uncomment the following code line to inspect x/y parameters:
+
+	// PS.debug( "PS.enter() @ " + x + ", " + y + "\n" );
+
+	// Add code here for when the mouse cursor/touch enters a bead.
+};
+
+/*
+PS.exit ( x, y, data, options )
+Called when the mouse cursor/touch exits bead(x, y).
+This function doesn't have to do anything. Any value returned is ignored.
+[x : Number] = zero-based x-position (column) of the bead on the grid.
+[y : Number] = zero-based y-position (row) of the bead on the grid.
+[data : *] = The JavaScript value previously associated with bead(x, y) using PS.data(); default = 0.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+*/
+
+PS.exit = function( x, y, data, options ) {
+	// Uncomment the following code line to inspect x/y parameters:
+
+	// PS.debug( "PS.exit() @ " + x + ", " + y + "\n" );
+
+	// Add code here for when the mouse cursor/touch exits a bead.
+};
+
+/*
+PS.exitGrid ( options )
+Called when the mouse cursor/touch exits the grid perimeter.
+This function doesn't have to do anything. Any value returned is ignored.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+*/
+
+PS.exitGrid = function( options ) {
+	// Uncomment the following code line to verify operation:
+
+	// PS.debug( "PS.exitGrid() called\n" );
+
+	// Add code here for when the mouse cursor/touch moves off the grid.
+};
+
+/*
+PS.keyDown ( key, shift, ctrl, options )
+Called when a key on the keyboard is pressed.
+This function doesn't have to do anything. Any value returned is ignored.
+[key : Number] = ASCII code of the released key, or one of the PS.KEY_* constants documented in the API.
+[shift : Boolean] = true if shift key is held down, else false.
+[ctrl : Boolean] = true if control key is held down, else false.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+*/
+
+PS.keyDown = function( key, shift, ctrl, options ) {
+	// Uncomment the following code line to inspect first three parameters:
+
+	// PS.debug( "PS.keyDown(): key=" + key + ", shift=" + shift + ", ctrl=" + ctrl + "\n" );
+
+	// Add code here for when a key is pressed.
+};
+
+/*
+PS.keyUp ( key, shift, ctrl, options )
+Called when a key on the keyboard is released.
+This function doesn't have to do anything. Any value returned is ignored.
+[key : Number] = ASCII code of the released key, or one of the PS.KEY_* constants documented in the API.
+[shift : Boolean] = true if shift key is held down, else false.
+[ctrl : Boolean] = true if control key is held down, else false.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+*/
+
+PS.keyUp = function( key, shift, ctrl, options ) {
+	// Uncomment the following code line to inspect first three parameters:
+
+	// PS.debug( "PS.keyUp(): key=" + key + ", shift=" + shift + ", ctrl=" + ctrl + "\n" );
+
+	// Add code here for when a key is released.
+};
+
+/*
+PS.input ( sensors, options )
+Called when a supported input device event (other than those above) is detected.
+This function doesn't have to do anything. Any value returned is ignored.
+[sensors : Object] = A JavaScript object with properties indicating sensor status; see API documentation for details.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+NOTE: Currently, only mouse wheel events are reported, and only when the mouse cursor is positioned directly over the grid.
+*/
+
+PS.input = function( sensors, options ) {
+	// Uncomment the following code lines to inspect first parameter:
+
+	//	 var device = sensors.wheel; // check for scroll wheel
+	//
+	//	 if ( device ) {
+	//	   PS.debug( "PS.input(): " + device + "\n" );
+	//	 }
+
+	// Add code here for when an input event is detected.
+};
+
+/*
+PS.shutdown ( options )
+Called when the browser window running Perlenspiel is about to close.
+This function doesn't have to do anything. Any value returned is ignored.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+NOTE: This event is generally needed only by applications utilizing networked telemetry.
+*/
+
+PS.shutdown = function( options ) {
+	// Uncomment the following code line to verify operation:
+
+	// PS.debug( "“Dave. My mind is going. I can feel it.”\n" );
+
+	// Add code here to tidy up when Perlenspiel is about to close.
+};
+
